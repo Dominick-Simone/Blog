3fc9d6399fe6015831e5ba82c0e454231f751301
Dominick Simone <domsimone123@gmail.com>
2021-09-28 17:41:24 -0400

    fixing heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
-const User = require('./Users');
+const User = require('./users');
 const Post = require('./Posts');
-const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -11,22 +10,5 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
-});
 
-Comment.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-Comment.belongsTo(Post, {
-  foreignKey: "post_id"
-})
-
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE'
-});
-
-module.exports = { User, Post, Comment };
+module.exports = { User, Post };
